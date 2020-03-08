6e643c262bfc8e52ebb1d75103774f5343b85e15
RichiePalma <palmamendoza.5@hotmail.com>
2020-03-08 13:25:07 -0600

    Solve go-functions-methods lab
    Signed-off-by: RichiePalma <palmamendoza.5@hotmail.com>

--- labs/go-functions-methods/geometry.go
+++ labs/go-functions-methods/geometry.go
@@ -5,9 +5,16 @@
 
 // Package geometry defines simple types for plane geometry.
 //!+point
-package geometry
+package main
 
-import "math"
+import (
+	"math"
+	"fmt"
+	"math/rand"
+	"time"
+	"os"
+	"strconv"
+)
 
 type Point struct{ X, Y float64 }
 
@@ -28,15 +35,59 @@ func (p Point) Distance(q Point) float64 {
 // A Path is a journey connecting the points with straight lines.
 type Path []Point
 
+func RandCoord(min int,max int) float64 {
+	rand.Seed(time.Now().UTC().UnixNano())
+	//return math.Round(rand.Float64()*float64((max-min) + min)/10)
+	return rand.Float64()*float64((max-min) + min)
+}
+
+func genVertices(sides int) Path {
+	v := make(Path,sides)
+	fmt.Printf("- Generating a [%d] sides figure\n",sides)
+	fmt.Println("- Figure's vertices")
+	for i:= 0; i < sides; i++ {
+		x := RandCoord(-100,100)
+		y := RandCoord(-100,100)
+		fmt.Printf("\t- (  %f,  %f)\n",x,y)
+		v[i] = Point{x,y}
+	}
+	return v
+}
+
 // Distance returns the distance traveled along the path.
 func (path Path) Distance() float64 {
 	sum := 0.0
+	dist := 0.0
+	fmt.Println("- Figure's Perimeter")
 	for i := range path {
-		if i > 0 {
-			sum += path[i-1].Distance(path[i])
+		if i == 1{
+			dist = path[i-1].Distance(path[i])
+			sum += dist
+			fmt.Printf("\t- %f", dist)
+		}else if i > 1 {
+			dist = path[i-1].Distance(path[i])
+			sum += dist
+			fmt.Printf("+ %f",dist)
 		}
 	}
+	dist = path[len(path)-1].Distance(path[0])
+	sum += dist // Last path, assuming figure is closed
+	fmt.Printf("+ %f = %f\n",dist,sum)
 	return sum
 }
 
+func main(){
+	if len(os.Args) != 2{
+		fmt.Println("Wrong run format: ./geometry {sides}")
+		os.Exit(2)
+	}
+
+	sides, _:= strconv.Atoi(os.Args[1])
+	if sides < 3{
+		fmt.Println("Sides should be above 2")
+	}else{
+		p := genVertices(sides)
+		p.Distance()
+	}
+}
 //!-path
